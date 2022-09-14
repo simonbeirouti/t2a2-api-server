49e83e6ceee24851b267cf9f4ddba2933d333163
simonbeirouti <hello@simonbeirouti.com>
2022-09-14 11:23:48 +1000

    add SQLAlchemy and Marshmallow objects

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.app_config')
+
+    db.init_app(app)
+    ma.init_app(app)
 
     return app
