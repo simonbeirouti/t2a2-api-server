8d31d8273b2456e6b58ddbe81594a2be4a50caf6
simonbeirouti <hello@simonbeirouti.com>
2022-09-14 14:01:52 +1000

    add bcrypt object

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy() # Database object
 ma = Marshmallow() # Marshmallow object
+bc = Bcrypt() # Bcrypt object
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +13,7 @@ def create_app():
 
     db.init_app(app)
     ma.init_app(app)
+    bc.init_app(app)
 
     from commands import db_cmd
     app.register_blueprint(db_cmd)
